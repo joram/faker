777cd59c48dd04f5e84d694d079b76dc328db973
John Oram <john@oram.ca>
2023-07-17 20:04:46 -0700

    switch to testing dates in unix timestamps

--- faker/age_test.go
+++ faker/age_test.go
@@ -28,22 +28,22 @@ func TestGetAge(t *testing.T) {
 func TestGetBirthday(t *testing.T) {
 	type test struct {
 		seed           int64
-		expectedOutput string
+		expectedOutput int64
 	}
 	tests := []test{
 		{
 			seed:           0,
-			expectedOutput: "1989-03-12 06:59:12 -0800 PST",
+			expectedOutput: 605717952,
 		},
 		{
 			seed:           1,
-			expectedOutput: "2009-09-16 20:32:31 -0700 PDT",
+			expectedOutput: 1253158351,
 		},
 	}
 	for _, test := range tests {
 		birthday := GetBirthday(test.seed)
-		if birthday.String() != test.expectedOutput {
-			t.Errorf("Expected birthday to be %s but got %s\n", test.expectedOutput, birthday)
+		if birthday.Unix() != test.expectedOutput {
+			t.Errorf("Expected birthday to be %d but got %.d\n", test.expectedOutput, birthday.Unix())
 		}
 	}
 }
